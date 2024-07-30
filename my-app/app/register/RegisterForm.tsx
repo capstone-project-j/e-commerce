8bd6c6551f05ad628cfa142ab7d526dc91845935
Jordy <jordy.lopez2003@gmail.com>
2024-07-30 10:14:06 -0400

    sign up page start

---
+++ my-app/app/register/RegisterForm.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Heading from "../components/Heading";
+
+const RegisterForm = () => {
+    return (
+        <>
+            <Heading title="Sign up for J~Shop" />
+            <hr className="bg-slate-300 w-full h-px" />
+        </>
+    );
+}
+
+export default RegisterForm;
